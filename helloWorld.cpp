cec5b28cb66a632f45a1d2cf6d8aa7e2dae328e3
amadeus84 <amadeus84@verizon.net>
2016-03-22 19:01:20 -0400

    Added files via upload

---
+++ helloWorld.cpp
@@ -0,0 +1,13 @@
+//
+// g++ -g -Wall -o helloWorld helloWorld.cpp
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char * argv[])
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
